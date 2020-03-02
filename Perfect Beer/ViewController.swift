1192da17a6d2fa62ef3104fb684d1187162b218d
Jon Gonzalez <jongonzalez000@gmail.com>
2020-03-02 18:59:20 +0100

    Project created, gitignore added

---
+++ Perfect Beer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Perfect Beer
+//
+//  Created by Jon Gonzalez on 02/03/2020.
+//  Copyright © 2020 Jon Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
